0f006e2f1430a8592911ef499cdfb890493d5dee
juhok <juho.kastemaa@gmail.com>
2016-02-24 11:57:11 +0100

    changes to js

--- js/view/detailsView.js
+++ js/view/detailsView.js
@@ -1,24 +1,29 @@
 //ExampleView Object constructor
 var DetailsView = function (container, model) {
-	
+	var row = " <tr> <td>Olive Oil</td> </tr>";
+
 	// Get all the relevant elements of the view (ones that show data
     // and/or ones that responed to interaction)
 
     this.backToSelectDish = container.find("#backToSelectDish");
+    this.ingTable = container.find("#ingTable");
 
-    console.log (model.getAllIngredients());
+  //  console.log (model.getAllIngredients());
 
-    this.totalCost.html(row);
-row=
-"
-                    <tr>
+    this.ingTable.html(row);
+
+
+/*
+
+<tr>
+
                           <td>"+unit+"</td>
                           <td>Olive Oil</td>
                           <td>SEK</td>
                           <td>0.20</td>
                       </tr>
 "
-    	
+  */  	
 	
     // View 4/6
     // Implement in Screens for Details
